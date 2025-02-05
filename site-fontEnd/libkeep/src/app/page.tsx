1debf96b25711c9ed18ad6e8b329bc07ad5bf10f
lelelol <leandrofortunatojf@gmail.com>
2025-02-05 20:44:47 -0300

    (quase) Termina sidebar

--- site-fontEnd/libkeep/src/app/page.tsx
+++ site-fontEnd/libkeep/src/app/page.tsx
@@ -1,7 +1,8 @@
+import Sidebar from "@/components/ui/sidebar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <></>
+    <Sidebar caminhoimagem="/logo.svg" nomeusuario="lele" />
   );
 }
--- site-fontEnd/libkeep/tailwind.config.ts
+++ site-fontEnd/libkeep/tailwind.config.ts
@@ -11,6 +11,7 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        verde: "#709A7C"
       },
     },
   },
