ff13b3247a0c0ecf982c6ee6a296f9550b4c350a
lelelol <leandrofortunatojf@gmail.com>
2025-02-26 20:16:36 -0300

    melhora tratamento

--- site-fontEnd/keeplit/src/app/page.tsx
+++ site-fontEnd/keeplit/src/app/page.tsx
@@ -1,15 +1,13 @@
+'use client';
 import Criaposts from "@/components/ui/criapost";
 import Postcard from "@/components/ui/postcard";
 import Sidebar from "@/components/ui/sidebar";
-//import { get } from "http";
-//import Image from "next/image";
 import { FiSearch } from "react-icons/fi";
 import { setupAPIClient } from './services/api';
-//const axios = require('axios');
 
 const axios = setupAPIClient();
 async function getPosts() {
-  
+
   try {
     const response = await axios.get('/posts');
     return (response.data);
@@ -41,9 +39,17 @@ export default async function Home() {
           <FiSearch color="black" className="w-8 h-8" />
           <input type="text" placeholder="Pesquisar" className=" bg-[#F8EEE3] text-black w-full p-2   rounded-2xl focus:outline-none text-2xl" />
         </div>
-        {posts.map((post: { nomeusuario: string; perfil: string; capa: string; titulo: string; titulolivro: string; rating: number; descricao: string; id: number }) => (
-
-          <Postcard key={post.id} nomeusuario={post.nomeusuario} perfil={post.perfil} capa={post.capa} titulo={post.titulo} titulolivro={post.titulolivro} rating={post.rating} descricao={post.descricao} />
+        {Array.isArray(posts) && posts.map((post) => (
+          <Postcard
+            key={post.id}
+            nomeusuario={post.nomeusuario}
+            perfil={post.perfil}
+            capa={post.capa}
+            titulo={post.titulo}
+            titulolivro={post.titulolivro}
+            rating={post.rating}
+            descricao={post.descricao}
+          />
         ))}
 
       </div>
--- site-fontEnd/keeplit/src/app/services/api.ts
+++ site-fontEnd/keeplit/src/app/services/api.ts
@@ -1,31 +1,32 @@
 import axios, { AxiosError } from 'axios';
 import { parseCookies } from 'nookies';
-import {AuthTokenError} from './errors/AuthTokenError';
-
+import { AuthTokenError } from './errors/AuthTokenError';
+import http from "http";
+import https from "https";
 import { signOut } from '../contexts/AuthContext';
 
-export function setupAPIClient(ctx = undefined){
+export function setupAPIClient(ctx = undefined) {
     const cookies = parseCookies(ctx);
 
     const api = axios.create({
         baseURL: 'http://localhost:8080',
         headers: {
             Authorization: `Bearer ${cookies['@leblanc.token']}`
-        }
+        },
     });
 
     api.interceptors.response.use(response => {
         return response;
     }, (err: AxiosError) => {
-        if(err.response && err.response.status === 401){
-            if(typeof window !== undefined){
+        if (err.response && err.response.status === 401) {
+            if (typeof window !== undefined) {
                 signOut();
-            }else{
+            } else {
                 return Promise.reject(AuthTokenError);
-            } 
+            }
         }
         return Promise.reject(err);
     });
-    
+
     return api;
 }
